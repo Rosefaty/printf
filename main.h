0cbf412fd9703fea4bd8d13d9db271b07f2ce940
Rosefaty <kokifoufi@gmail.com>
2023-04-24 01:28:53 -0700

    main.h

--- main.h
+++ main.h
@@ -1,16 +1,69 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
 
-/* Libraries */
-#include <stdio.h>
-#include <stdlib.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
-/* Function prototypes */
-int _putchar(char c);
-void function1(void);
-int function2(int arg1, char arg2);
-float function3(char arg1, float arg2, int arg3);
-/* ... */
+/**
+ * _printf - prints output according to a format
+ * @format: character string with directives
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
 
-#endif /* MAIN_H */
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				count += _putchar(c);
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				int i;
+				for (i = 0; str[i]; i++)
+				{
+					count += _putchar(str[i]);
+				}
+			}
+			else if (*format == '%')
+			{
+				count += _putchar('%');
+			}
+			else
+			{
+				/* ignore invalid directives */
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+
+		format++;
+	}
+
+	va_end(args);
+
+	return (count);
+}
 

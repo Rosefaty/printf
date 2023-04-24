b0553e34dce552302f284da4cec0b175d308d2dd
Rosefaty <kokifoufi@gmail.com>
2023-04-24 01:23:37 -0700

    usu

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+int _putchar(char c);
+void function1(void);
+int function2(int arg1, char arg2);
+float function3(char arg1, float arg2, int arg3);
+/* ... */
+
+#endif /* MAIN_H */
+
